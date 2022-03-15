e99d2fae897fef46c896edce7cf05afd75a6eb12
SebasttianM <sebasttianvmarin@gmail.com>
2022-03-15 00:46:58 -0500

    f

--- script/math.js
+++ script/math.js
@@ -1,8 +1,22 @@
+const n1= document.getElementById('1')
+const n2= document.getElementById('2')
+const n3= document.getElementById('3')
+const n4= document.getElementById('4')
+const n5= document.getElementById('5')
+const n6= document.getElementById('6')
 const n7= document.getElementById('7')
+const n8= document.getElementById('8')
+const n9= document.getElementById('9')
+const n0= document.getElementById('0')
 const resultado= document.getElementById('resultado')
 const sum = document.getElementById('+')
-const equal= document.getElementById('=')
+const sub = document.getElementById('-')
+const dot= document.getElementById('.')
+const mult= document.getElementById('X')
+const div= document.getElementById('/')
 const reset= document.getElementById('reset')
+const calc = document.getElementById('calculator')
+const equal = document.getElementById('=')
 
 reset.addEventListener('click',()=>{
     resultado.value=''
@@ -17,6 +31,7 @@ const hacerOperacion= ()=>{
         console.log(result);
         let operacion= eval(result)
         console.log(operacion);
+        resultado.value= operacion
     })
 }
 hacerOperacion()
@@ -26,7 +41,18 @@ const recibirValor= (numero)=>{
         resultado.value+= numero.value;
     })
 }
+
+recibirValor(n1)
+recibirValor(n2)
+recibirValor(n3)
+recibirValor(n4)
+recibirValor(n5)
+recibirValor(n6)
 recibirValor(n7)
+recibirValor(n8)
+recibirValor(n9)
+recibirValor(n0)
+
 recibirValor(sum)
 
 
@@ -38,10 +64,11 @@ recibirValor(sum)
 
 
 
+
 const body= document.querySelector('body')
-const tema1= document.getElementById('tema1')
-const tema2= document.getElementById('tema2')
-const tema3= document.getElementById('tema3')
+const tema1= document.getElementById('t1')
+const tema2= document.getElementById('t2')
+const tema3= document.getElementById('t3')
 
 tema1.addEventListener('click',()=>{
     body.classList.add('active1')
